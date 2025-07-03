6257b15db9de1fef1cd678a336fd7853e4627091
zaebee <zaebuntu@gmail.com>
2025-07-03 19:56:39 +0300

    chore: fix linter.

--- format.sh
+++ format.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-npx prettier --write "**/*.{json,css,html}"
-npx clang-format -i proto/*.proto
+npm run lint
+# npx prettier --write "**/*.{json,css,html,js,jsx,ts,tsx,md,mdx}"
+# npx clang-format -i proto/*.proto
